a43568c09f0d07527ff06c5fb56cd8cf24bee19a
andrewyuau <andrewyu.au@gmail.com>
2018-01-23 10:21:49 +1100

    app

--- processing/analysis/analysis.js
+++ processing/analysis/analysis.js
@@ -261,7 +261,7 @@ function analyzeImage(args, fileName, analyzeCallback) {
             method: 'POST',
             url: 'https://gateway-a.watsonplatform.net/visual-recognition/api/v3/classify' + // eslint-disable-line
               '?api_key=' + args.watsonApiKey +
-              '&classifier_ids=SewerSpy_880630111' +
+              '&classifier_ids=food' +
               '&version=2016-05-20',
             headers: {
               'Content-Length': fs.statSync(fileName).size
@@ -277,7 +277,7 @@ function analyzeImage(args, fileName, analyzeCallback) {
             }
             callback(null);
           }));
-      }
+    }
   ],
   (err) => {
     analyzeCallback(err, analysis);
